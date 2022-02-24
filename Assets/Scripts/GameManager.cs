c921c87fccddf1066169733781186b8a2f8c1a26
yeeunsy <90146746+yeeunsy@users.noreply.github.com>
2022-02-24 20:36:54 +0900

    lastcm

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public int[] jellyGoleList;
+    public int[] jellyGoldList;
+    public int[] jellyGelatinList;
+    public Sprite[] jellySpriteList;
+    public string[] jellyNameList;
+
     public Vector3[] PointList;
+
     public RuntimeAnimatorController[] LvAc;
 
     public void ChangeAc(Animator anim, int level)
